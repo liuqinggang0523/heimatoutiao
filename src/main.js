1d1e2f45093f308eb53ecb607e125f98ed7bc747
liuqinggang <1542574711@qq.com>
2019-12-18 20:02:26 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'// 引入ElementUI
+import './element-ui/lib/theme-chalk/index.css'
+import './styles/index.less'// 引入初始化样式
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
